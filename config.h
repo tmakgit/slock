b37485e4748671bb582bf436755bc6a3a996505c
Tyler McClanahan <tmak1787@gmail.com>
2026-04-02 16:30:51 -0600

    colors

--- config.h
+++ config.h
@@ -3,9 +3,9 @@ static const char *user  = "tmak";
 static const char *group = "tmak";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#505354",     /* after initialization */
-	[INPUT] =  "#505354",   /* during input */
-	[FAILED] = "#F92672",   /* wrong password */
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "steelblue",   /* during input */
+	[FAILED] = "red3",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
